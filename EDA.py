4c29608650e87b7bbf363c9de185882352b1c85a
Akarsh Bansal <akarshsanjeevbansal@gmail.com>
2018-04-26 17:57:55 -0500

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -36,7 +36,7 @@ for i,j in enumerate(player_datahist.select_dtypes(include = ['float64','int64']
     sns.distplot(pat.loc[:,j],kde = False,hist = True, ax = ax2[int(i/6)][i%6])
 fighist.tight_layout()
 
-
+#Creating Boxplot
 figbox, ax2 = plot.subplots(nrows=7,ncols=6)
 figbox.set_size_inches(20,16)
 for i,j in enumerate(player_datahist.select_dtypes(include = ['float64','int64']).columns.tolist()):
