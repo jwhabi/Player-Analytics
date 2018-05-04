682e055643ddf00ef2df7034b724d57b1090cddb
Pragya2393-mishra <32080490+Pragya2393-mishra@users.noreply.github.com>
2018-05-03 22:03:27 -0500

    Clean up and comments update

--- Final_feature extraction.py
+++ Final_feature extraction.py
@@ -8,9 +8,6 @@ Created on Thu Apr 2 18:22:57 2018
 import sqlite3 
 import pandas as pd
 import numpy as np
-import itertools
-
-import os
 import matplotlib.pyplot as plt
 import datetime
 from IPython import get_ipython
@@ -213,7 +210,6 @@ pos=[]
 bucket810 =[]
 bucket1013=[]
 bucket1316=[]
-bucket_total=[]
 for i in range(0,len(df['Player_Attributes'])):
     
     result= get_position(df['Player_Attributes'].player_api_id[i])
@@ -238,54 +234,9 @@ bucket2 = np.array(bucket2,dtype=float)
 bucket3 = np.array(bucket3,dtype=float)  
 
 
-
-#calculating diff12, diff23 and diff31
-diff12= []
-diff23=[]
-diff31 =[]
-for i in range(0,len(bucket1)):
-
-    if ((bucket1[i]==np.nan) and (bucket2[i]== np.nan)):
-        diff12.append(np.nan)
-        diff23.append(np.nan)
-        diff31.append(np.nan)
-        
-    elif ((bucket2[i]==np.nan) and (bucket3[i]== np.nan)):
-        diff12.append(np.nan)
-        diff23.append(np.nan)
-        diff31.append(np.nan)
-        
-    elif ((bucket1[i]==np.nan) and (bucket3[i]== np.nan)):
-        diff12.append(np.nan)
-        diff23.append(np.nan)
-        diff31.append(np.nan)
-        
-    elif (bucket1[i]==np.nan):
-        diff12.append(np.nan)
-        diff23.append(abs(bucket2[i]-bucket3[i]))
-        diff31.append(np.nan)
-    elif (bucket2[i]==np.nan):
-        diff12.append(np.nan)
-        diff23.append(np.nan)
-        diff31.append(abs(bucket1[i]-bucket3[i]))
-    elif (bucket3[i]==np.nan):
-        diff12.append(abs(bucket1[i]-bucket2[i]))
-        diff23.append(np.nan)
-        diff31.append(np.nan)
-    else:
-        diff12.append(abs(bucket1[i]-bucket2[i]))
-        diff23.append(abs(bucket2[i]-bucket3[i]))
-        diff31.append(abs(bucket3[i]-bucket1[i])) 
-        
- 
-diff1 = np.array(diff12, dtype=float) 
-diff2 = np.array(diff23, dtype=float)
-diff3 = np.array(diff31, dtype=float)
-
-
 bucket_mean1=[]
 bucket_std1 =[]
-average_deviation=[]
+
 for i in range(0,len(bucket3)):
     arr=[]
     arr_diff=[]
@@ -296,40 +247,19 @@ for i in range(0,len(bucket3)):
     arr_mean = np.nanmean(arr)
     bucket_mean1.append(arr_mean)    
     bucket_std1.append(np.nanstd(arr))
-    
-    arr_diff.append(diff1[i])
-    arr_diff.append(diff2[i])
-    arr_diff.append(diff3[i])
     arr_diff=np.array(arr_diff,dtype=np.float)
-    arr_diff_mean = np.nanmean(arr_diff)
-    average_deviation.append(arr_diff_mean)
+
     
 bucket_mean1[0]
 bucket_std1[0]
-diff1_std = np.nanstd(diff1)
-diff2_std = np.nanstd(diff2)
-diff3_std = np.nanstd(diff3)
 
-sr=[]
-for i in range(0, len(bucket1)):
-    sr.append(i)
 
-#Adding bucket810, bucket1013, bucket1316, bucket_mean1, diff12, diff23, diff31, diff_mean to "Player_Attributes" 
+#Adding bucket1, bucket2, bucket3, bucket_mean1, bucket_std1 to "Player_Attributes" 
 df['Player_Attributes']['bucket810'] = bucket1
 df['Player_Attributes']['bucket1013'] = bucket2
 df['Player_Attributes']['bucket1316'] = bucket3
-df['Player_Attributes']['bucket_total'] = bucket_total
-df['Player_Attributes']['diff12'] = diff12
-df['Player_Attributes']['diff23'] = diff23
-df['Player_Attributes']['diff31'] = diff31
-df['Player_Attributes']['diff_mean'] = diff_mean
 df['Player_Attributes']['bucket_mean1'] = bucket_mean1
 df['Player_Attributes']['bucket_std1'] = bucket_std1
-df['Player_Attributes']['diff1'] = diff1
-df['Player_Attributes']['diff2'] = diff2
-df['Player_Attributes']['diff3'] = diff3
-df['Player_Attributes']['average_deviation'] = average_deviation
-df['Player_Attributes']['sr'] = sr
 df['Player_Attributes'].to_csv('Player_Attributes_new_v22' + '.csv', index_label='index') 
 
 # Grouping and labelling:
@@ -347,6 +277,8 @@ print('saving figure')
 fig.savefig('bucket_mean1_hist.png')
 plt.close(fig)
 
+
+#Using k-means clustering to partition data into 4 parts for each position
 len(bucket_mean1.reshape(-1,1))
 
 from sklearn.cluster import KMeans
@@ -354,15 +286,10 @@ kmeans = KMeans(n_clusters=4, random_state=0)
 kmeans.fit(bucket_mean1.reshape(-1,1))
 bucket_mean['kmean_predict'] = kmeans.predict(bucket_mean1.reshape(-1,1))
 
-bucket_mean.to_csv('F:/Player-Analytics/kmean_predicted_on_bucket_mean.csv')
-
 bucket_mean['kmeans_predict_position'] =np.where((bucket_mean['kmean_predict']==0), 'for',bucket_mean['kmean_predict'])
 bucket_mean['kmeans_predict_position'] =np.where((bucket_mean['kmean_predict']==1), 'def',bucket_mean['kmeans_predict_position'])
 bucket_mean['kmeans_predict_position'] =np.where((bucket_mean['kmean_predict']==2), 'mid',bucket_mean['kmeans_predict_position']) 
 bucket_mean['kmeans_predict_position'] =np.where((bucket_mean['kmean_predict']==3), 'gk',bucket_mean['kmeans_predict_position'])
 
 bucket_mean.kmeans_predict_position.value_counts()
-
-
-
-
+bucket_mean.to_csv('F:/Player-Analytics/kmean_predicted_on_bucket_mean.csv')
