f1d29ffee12bf871e32c8f90466a9aad52e75942
Jaideep <jaideepwhabi@gmail.com>
2018-05-03 22:16:50 -0500

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,28 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr  4 22:34:45 2018
+Created on Thu Apr 16 19:04:16 2018
 
-@author: jaide
+@author: pragy,jaide
 """
 
 import sqlite3 
 import pandas as pd
 import numpy as np
-
-import os
 import matplotlib.pyplot as plt
 import datetime
 from IPython import get_ipython
 get_ipython().run_line_magic('matplotlib', 'inline')
 
-conn = sqlite3.connect("C:\\Users\\jaide\\Downloads\\soccer\\database.sqlite")
-
+conn = sqlite3.connect("F:\\DPA\\Project\\database.sqlite")
 cur=conn.cursor()
-
-
 #------------------------
 table = pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table'", conn)
-
 df={}
 for n in table['name']:
     print(n)
@@ -33,73 +27,39 @@ for n in table['name']:
 #locals().update(df) ------->>> is messing with local namespace a bad idea?? research needed
 print(df.keys())
 
-#Evaluate the labelled position for each player based on match lineups
-def get_position(x):
-    #data=df['Player_Attributes'][df['Player_Attributes'].player_api_id==x]
-    #print(data['player_api_id'])
-    d2=df['Match'].home_player_Y1[df['Match'].home_player_1==x]
-    d2=d2.append(df['Match'].home_player_Y2[df['Match'].home_player_2==x])
-    d2=d2.append(df['Match'].home_player_Y3[df['Match'].home_player_3==x])
-    d2=d2.append(df['Match'].home_player_Y4[df['Match'].home_player_4==x])
-    d2=d2.append(df['Match'].home_player_Y5[df['Match'].home_player_5==x])
-    d2=d2.append(df['Match'].home_player_Y6[df['Match'].home_player_6==x])
-    d2=d2.append(df['Match'].home_player_Y7[df['Match'].home_player_7==x])
-    d2=d2.append(df['Match'].home_player_Y8[df['Match'].home_player_8==x])
-    d2=d2.append(df['Match'].home_player_Y9[df['Match'].home_player_9==x])
-    d2=d2.append(df['Match'].home_player_Y10[df['Match'].home_player_10==x])
-    d2=d2.append(df['Match'].home_player_Y11[df['Match'].home_player_11==x])
-    
-    d2=d2.append(df['Match'].away_player_Y1[df['Match'].away_player_1==x])
-    d2=d2.append(df['Match'].away_player_Y2[df['Match'].away_player_2==x])
-    d2=d2.append(df['Match'].away_player_Y3[df['Match'].away_player_3==x])
-    d2=d2.append(df['Match'].away_player_Y4[df['Match'].away_player_4==x])
-    d2=d2.append(df['Match'].away_player_Y5[df['Match'].away_player_5==x])
-    d2=d2.append(df['Match'].away_player_Y6[df['Match'].away_player_6==x])
-    d2=d2.append(df['Match'].away_player_Y7[df['Match'].away_player_7==x])
-    d2=d2.append(df['Match'].away_player_Y8[df['Match'].away_player_8==x])
-    d2=d2.append(df['Match'].away_player_Y9[df['Match'].away_player_9==x])
-    d2=d2.append(df['Match'].away_player_Y10[df['Match'].away_player_10==x])
-    d2=d2.append(df['Match'].away_player_Y11[df['Match'].away_player_11==x])
-    
-    #print(d2)
-    if len(d2) > 0:
-                Y = np.array(d2,dtype=np.float)
-                mean_y = np.nanmean(Y)
-                print(mean_y)
-                if (mean_y >= 10.0):
-                    return "for"
-                elif (mean_y > 5):
-                    return "mid"
-                elif (mean_y > 1):
-                    return "def"
-                elif (mean_y == 1.0):
-                    return "gk"
-    return None
-        
-#Test: (uncomment any below lines to test the function)
-#get_position(36835)    
-#get_position(38788)
-#get_position(94462)
-#get_position(37069)
-#get_position(50160)
-pos=[]
+#Adding extracted position to "Player_Attributes"
 
+df['Player_Attributes'].shape
+p= pd.read_csv('F:/Player-Analytics/kmean_predicted_on_bucket_mean.csv')
+list(p)
+p1=p.drop('bucket_mean1',1)
+p1=p1.drop('bucket_std1',1)
+p1=p1.drop('kmean_predict',1)
+p1=p1.drop('Unnamed: 0',1)
+p1=p1.drop( 'index',1)
+p1['position']=p1['kmeans_predict_position']
+p1.position
+p1=p1.drop('kmeans_predict_position',1)
+list(p1)
 
-for i in range(0,len(df['Player_Attributes'])):
-    pos.append(get_position(df['Player_Attributes'].player_api_id[i]))
-    
-df['Player_Attributes']['position'] = pos
-df['Player_Attributes'].to_csv('Player_Attributes' + '.csv', index_label='index')   
+r= np.unique(p.player_api_id, return_index=True, return_inverse=True, return_counts=True)
+
+df['Player_Attributes'] =p1
+df['Player_Attributes'].shape
+df['Player_Attributes'].to_csv('Player_Attributes_new' + '.csv', index_label='index')   
 
 nanrows=df['Player_Attributes'][df['Player_Attributes'].isnull().T.any().T]
-nanrows.shape
-
+nanrows.shape # 0.02%
 df['Player_Attributes']=df['Player_Attributes'].dropna()
 df['Player_Attributes'].shape
-df['Player_Attributes'].to_csv('Player_Attributes' + '.csv', index_label='index')   
+df['Player_Attributes'].to_csv('Player_Attributes_new1' + '.csv', index_label='index')   
 
-#180195 rows (prev 183978)
-#10403 players (prev 11060)
+r1= np.unique(df['Player_Attributes'].player_api_id, return_index=True, return_inverse=True, return_counts=True)
+
+len(r1[0])
+
+#180354 rows (prev 183978)
+#10410 players (prev 11060)
 
 #Merge with additonal player data like birthdate,etc
 player_data=pd.merge(df['Player_Attributes'], df['Player'], how='inner', on=['player_api_id'])
@@ -131,23 +91,19 @@ This method would drop all the rows containing uncertain values and map the
 data to correct key as seen below
 """ 
 def prep_player_data (df):
-    #work_rate_dict = {'low': 'low', 'medium': 'medium', 'high': 'high'}
-    #pref_foot_dict = {'left': 'left', 'right': 'right', 'None': 'None'}
-    work_rate_dict = {'low': 0, 'medium': 1, 'high': 2}
-    pref_foot_dict = {'left': 1, 'right': 2, 'None': 0}
-    pos_dict = {'for': 3, 'mid': 2, 'def': 1, 'gk': 0}
+    work_rate_dict = {'low': 'low', 'medium': 'medium', 'high': 'high'}
+    pref_foot_dict = {'left': 'left', 'right': 'right', 'None': 'None'}
 
     df = df.loc[(df['attacking_work_rate'].isin(work_rate_dict.keys())) & 
                 (df['defensive_work_rate'].isin(work_rate_dict.keys()))].copy()
     
     df.loc[:, 'preferred_foot'] = df.loc[:, 'preferred_foot'].map(pref_foot_dict)
-    df.loc[:, 'position'] = df.loc[:, 'position'].map(pos_dict)
     df.loc[:, 'attacking_work_rate'] = df.loc[:, 'attacking_work_rate'].map(work_rate_dict)
     df.loc[:, 'defensive_work_rate'] = df.loc[:, 'defensive_work_rate'].map(work_rate_dict)
     
     return df
 
-player_data=prep_player_data(player_data) #176002 rows
+player_data=prep_player_data(player_data) #176161 rows
 player_data.shape
 print(player_data.head(1))
 player_data['player_api_id'].astype('category')
@@ -165,17 +121,12 @@ print(player_data.head(1))
 
 player_data['player_fifa_api_id']=player_data['player_fifa_api_id'].astype(np.int64)
 player_data['player_api_id']=player_data['player_api_id'].astype(np.int64)
-player_data['date']=player_data['date'].astype(np.str) ##Future warning??
+player_data['date']=player_data['date'].astype(np.datetime64) ##Future warning??
 player_data['overall_rating']=player_data['overall_rating'].astype(np.int16)
 player_data['potential']=player_data['potential'].astype(np.int16)
-
-#player_data['preferred_foot']=player_data['preferred_foot'].astype('category')
-#player_data['attacking_work_rate']=player_data['attacking_work_rate'].astype('category')
-#player_data['defensive_work_rate']=player_data['defensive_work_rate'].astype('category')
-player_data['preferred_foot']=player_data['preferred_foot'].astype(np.int16)
-player_data['attacking_work_rate']=player_data['attacking_work_rate'].astype(np.int16)
-player_data['defensive_work_rate']=player_data['defensive_work_rate'].astype(np.int16)
-
+player_data['preferred_foot']=player_data['preferred_foot'].astype('category')
+player_data['attacking_work_rate']=player_data['attacking_work_rate'].astype('category')
+player_data['defensive_work_rate']=player_data['defensive_work_rate'].astype('category')
 player_data['crossing']=player_data['crossing'].astype(np.int16)
 player_data['finishing']=player_data['finishing'].astype(np.int16)
 player_data['heading_accuracy']=player_data['heading_accuracy'].astype(np.int16)
@@ -216,9 +167,11 @@ player_data['weight']=player_data['weight'].astype(np.int16)
 player_data['age']=player_data['age'].astype(np.int16)
 player_data['position']=player_data['position'].astype('category')
 
+#Dropping duplicate rows
+player_data=player_data.drop_duplicates()
 
 player_data['attacking_work_rate'].values
-player_data.shape
+len(player_data)
 
 for col in player_data.columns:
     print(col)
@@ -243,175 +196,15 @@ for col in player_data.columns:
         fig.savefig(col+'.png')
         plt.close(fig)
         
-player_data.describe(include= 'all')
-player_data.dtypes
-      
 from sklearn.model_selection import train_test_split
 X=player_data
-X=X.drop(['position'],axis=1)
-y=player_data.position
+y=player_data.overall_rating
 X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=42)
 
 X_train.shape
 X_train.boxplot('potential')
-X_train.to_csv('training set.csv')
-X_test.shape
+X_train.to_csv('F:/Player-Analytics/training set_new1.csv')
+X_test
 X_test.boxplot('potential')
-X_test.to_csv('test set.csv')
-
-X_train=X_train.drop('date',1)
-X_test=X_test.drop('date',1)
-X_train=X_train.drop('birthday',1)
-X_test=X_test.drop('birthday',1)
-X_train=X_train.drop('player_name',1)
-X_test=X_test.drop('player_name',1)
-X_test=X_test.drop('player_api_id',1)
-X_train=X_train.drop('player_api_id',1)
-X_test=X_test.drop('player_fifa_api_id',1)
-X_train=X_train.drop('player_fifa_api_id',1)
-
-X_test=X_test.drop('defensive_work_rate',1)
-X_train=X_train.drop('defensive_work_rate',1)
-X_test=X_test.drop('attacking_work_rate',1)
-X_train=X_train.drop('attacking_work_rate',1)
-X_test=X_test.drop('preferred_foot',1)
-X_train=X_train.drop('preferred_foot',1)
-
-from sklearn.naive_bayes import GaussianNB
-gnb = GaussianNB()
-y_pred = gnb.fit(reduced_data_pca, y_train).predict(reduced_data_pca1)
-y_pred.dtype
-print("Number of mislabeled points out of a total %d points : %d"  % (X_test.shape[0],(y_test != y_pred).sum()))
-from Eval import Eval
-eval1 = Eval(y_pred, np.array(y_test))
-
-print("Positive Class:")
-print("Accuracy: ",eval1.Accuracy())
-from sklearn.metrics import recall_score,precision_score,accuracy_score
-print(recall_score(y_test,y_pred,average=None))
-print(precision_score(y_test,y_pred,average=None))
-print(accuracy_score(y_test,y_pred))
-print("Recall: ",eval1.Recall())
-print("Precision: ",eval1.Precision())
-
-means=pd.DataFrame(columns=['Feature','diff_abs_mean','diff_var'])
-diff=[]
-ad=[]
-for i in range(0,gnb.theta_.shape[1]):
-    diff.append(abs(gnb.theta_[0,i]-gnb.theta_[1,i]))
-    ad.append(gnb.sigma_[0,i]+gnb.sigma_[1,i])
-    means=means.append([{'Feature': X_train.columns[i],'diff_abs_mean': abs(gnb.theta_[0,i]-gnb.theta_[1,i]), 'diff_var': gnb.sigma_[0,i]+gnb.sigma_[1,i]}])
-means['measure']=means.diff_abs_mean/means.diff_var
-means=means.reset_index()
-means=means.drop(['index','diff_abs_mean','diff_var'],1)
-means.sort_values(by=['measure'], ascending = False)
-
-
-from sklearn.model_selection import cross_val_score
-cross_fold = pd.DataFrame(columns=['Precision', 'Recall', 'Accuracy'])
-cross_fold.Precision=cross_val_score(gnb,X_train,y_train,cv=10,scoring='precision_micro')
-
-cross_fold.Recall=cross_val_score(gnb,X_train,y_train,cv=10,scoring='recall_micro')
-cross_fold.Accuracy=cross_val_score(gnb,X_train,y_train,cv=10)
-cross_fold
-
-#--------------------------------------------------------
-
-
-#def prep_player_data_reverse (df):
-#    #work_rate_dict = {'low': 'low', 'medium': 'medium', 'high': 'high'}
-#    #pref_foot_dict = {'left': 'left', 'right': 'right', 'None': 'None'}
-#    work_rate_dict = {0: 'low', 1: 'medium', 2: 'high'}
-#    pref_foot_dict = { 1:'left', 2:'right',  0: 'None'}
-#    
-#
-#    df = df.loc[(df['attacking_work_rate'].isin(work_rate_dict.keys())) & 
-#                (df['defensive_work_rate'].isin(work_rate_dict.keys()))].copy()
-#    
-#    df.loc[:, 'preferred_foot'] = df.loc[:, 'preferred_foot'].map(pref_foot_dict)
-#    
-#    df.loc[:, 'attacking_work_rate'] = df.loc[:, 'attacking_work_rate'].map(work_rate_dict)
-#    df.loc[:, 'defensive_work_rate'] = df.loc[:, 'defensive_work_rate'].map(work_rate_dict)
-#    df['attacking_work_rate']=df['attacking_work_rate'].astype('category')
-#    df['defensive_work_rate']=df['defensive_work_rate'].astype('category')
-#    df['preferred_foot']=df['preferred_foot'].astype('category')
-#    return df
-#
-#X_train=prep_player_data_reverse(X_train)
-#X_test=prep_player_data_reverse(X_test)
-#X_train.dtypes
-#X_test.dtypes
-#pos_dict = { 3: 'for',  2: 'mid', 1: 'def', 0: 'gk'}
-#df.loc[:, 'position'] = df.loc[:, 'position'].map(pos_dict)
-
-
-def convert_to_categories(x):
-    for i in x.columns:
-        if x[i].dtype=='int16':
-            category=pd.cut(np.array(x[i]), 3,labels=[0,1,2])
-            x[i]=category
-    
-    return x
-
-X_train=convert_to_categories(X_train)
-X_train.dtypes
-X_train.aggression
-
-
-X_test=convert_to_categories(X_test)
-X_test.dtypes
-X_test.aggression
-
-#y_train=y_train.map({ 3: 'for',  2: 'mid', 1: 'def', 0: 'gk'})
-#y_test=y_test.map({ 3: 'for',  2: 'mid', 1: 'def', 0: 'gk'})
-#
-#y_train=y_train.astype('category')
-#y_test=y_test.astype('category')
-
-
-from sklearn.naive_bayes import MultinomialNB
-gnb = MultinomialNB()
-y_pred = gnb.fit(X_train, y_train).predict(X_test)
-gnb.fit(X_train, y_train).predict(X_test.head(5))
-print("Number of mislabeled points out of a total %d points : %d"  % (X_test.shape[0],(y_test != y_pred).sum()))
-from Eval import Eval
-eval1 = Eval(y_pred, np.array(y_test))
-
-print("Positive Class:")
-print("Accuracy: ",eval1.Accuracy())
-from sklearn.metrics import recall_score,precision_score,accuracy_score
-print(recall_score(y_test,y_pred,average=None))
-print(precision_score(y_test,y_pred,average=None))
-print(accuracy_score(y_test,y_pred))
-print("Recall: ",eval1.Recall())
-print("Precision: ",eval1.Precision())
-
-
-from sklearn.decomposition import RandomizedPCA,PCA
-
-
-# Create a regular PCA model 
-pca = PCA(n_components=10)
-
-# Fit and transform the data to the model
-reduced_data_pca = pca.fit_transform(X_train)
-
-# Inspect the shape
-reduced_data_pca.shape
-
-# Print out the data
-
-#print(reduced_data_pca)
-
-
-pca = PCA(n_components=10)
-
-# Fit and transform the data to the model
-reduced_data_pca1 = pca.fit_transform(X_test)
-
-# Inspect the shape
-reduced_data_pca1.shape
-
-# Print out the data
-
-#print(reduced_data_pca)
+X_test.to_csv('F:/Player-Analytics/test set_new1.csv')
+player_data.to_csv('player_data.csv')
